ed54185c24214431f12ca6348de03f3c4c372037
salhinour <salhi.nour@esprit.tn>
2024-02-17 00:49:09 +0100

    back et front

--- src/Controller/CategorieBackCController.php
+++ src/Controller/CategorieBackCController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Categorie;
-use App\Form\Categorie1Type;
+use App\Form\CategorieType;
 use App\Repository\CategorieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,7 +26,7 @@ class CategorieBackCController extends AbstractController
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $categorie = new Categorie();
-        $form = $this->createForm(Categorie1Type::class, $categorie);
+        $form = $this->createForm(CategorieType::class, $categorie);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -53,7 +53,7 @@ class CategorieBackCController extends AbstractController
     #[Route('/{id}/edit', name: 'app_categorie_back_c_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Categorie $categorie, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(Categorie1Type::class, $categorie);
+        $form = $this->createForm(CategorieType::class, $categorie);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
